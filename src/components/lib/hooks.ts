590727abf68d5d90c29a790f917f2385e4ac6c47
Dorota Stefanik <dorota.stefanik@gmail.com>
2025-02-07 13:29:46 +0100

    Add react-query in job item.

--- src/components/lib/hooks.ts
+++ src/components/lib/hooks.ts
@@ -1,61 +1,42 @@
 import { useEffect, useState } from "react";
-import { JobItem } from "./types";
+import { JobItem, JobItemExpanded } from "./types";
 import { BASE_API_URL } from "./constants";
 import { useQuery } from "@tanstack/react-query";
 
-const fetchJobItem = async (id: number) => {
+type JobItemApiResponse = {
+  public: boolean;
+  jobItem: JobItemExpanded;
+};
+
+const fetchJobItem = async (id: number): Promise<JobItemApiResponse> => {
   const response = await fetch(`${BASE_API_URL}/${id}`);
+  // 4xx or 5xx
+  if (!response.ok) {
+    const errorData = await response.json();
+    throw new Error(errorData.description);
+  }
   const data = await response.json();
   return data;
 };
 
 export function useJobItem(id: number | null) {
-  const { data, isLoading } = useQuery(["job-item", id], () => (id ? fetchJobItem(id) : null), {
+  const { data, isInitialLoading } = useQuery(["job-item", id], () => (id ? fetchJobItem(id) : null), {
     staleTime: 1000 * 60 * 60,
     refetchOnWindowFocus: false,
     retry: false,
     enabled: Boolean(id),
-    onError: () => {},
+    onError: error => {
+      console.log(error);
+    },
   });
-  const jobItem = data.jobItem;
+  const jobItem = data?.jobItem;
+  const isLoading = isInitialLoading;
   return { jobItem, isLoading } as const;
 }
-
-// komponent bez użycia providara cachującego dane
-// export function useJobItem(id: number | null) {
-//   const [jobItem, setJobItem] = useState<JobItemExpanded | null>(null);
-//   const [isLoading, setIsLoading] = useState(false);
-
-//   useEffect(() => {
-//     if (!id) return;
-
-//     const fetchData = async () => {
-//       setIsLoading(true);
-//       const response = await fetch(`${BASE_API_URL}/${id}`);
-//       const data = await response.json();
-//       setIsLoading(false);
-//       setJobItem(data.jobItem);
-//     };
-
-//     fetchData();
-//   }, [id]);
-
-//   return { jobItem, isLoading } as const;
-// }
-
-// export function useActiveJobItem() {
-//   const activeId = useActiveId();
-//   const jobItem = useJobItem(activeId);
-
-//   return jobItem;
-// }
 
 export function useJobItems(searchText: string) {
   const [jobItems, setJobItems] = useState<JobItem[]>([]);
   const [isLoading, setIsLoading] = useState(false);
-
-  const totalNumberOfResults = jobItems.length;
-  const jobItemsSliced = jobItems.slice(0, 7);
 
   useEffect(() => {
     if (!searchText) return;
@@ -71,7 +52,7 @@ export function useJobItems(searchText: string) {
   }, [searchText]);
 
   // return {jobItemsSliced, isLoading};
-  return { jobItemsSliced, isLoading, totalNumberOfResults } as const;
+  return { jobItems, isLoading } as const;
   // jeśli zwrócimy tablicę trzeba pilnować kolejności elementów, dlatego zwrócenie obiektu jest bezpieczniejsze
 }
 
