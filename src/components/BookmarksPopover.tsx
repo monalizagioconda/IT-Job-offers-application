9e0cc3adcad6550d6c972a6ad9c02568c31790b2
Dorota Stefanik <dorota.stefanik@gmail.com>
2025-02-28 12:44:46 +0100

    Close popover on click outside by using useRef.

--- src/components/BookmarksPopover.tsx
+++ src/components/BookmarksPopover.tsx
@@ -1,12 +1,15 @@
+import { forwardRef } from "react";
 import JobList from "./JobList";
 import { useBookmarksContext } from "./lib/hooks";
 
-export default function BookmarksPopover() {
+const BookmarksPopover = forwardRef<HTMLDivElement>(function (_, ref) {
   const { bookmarkedJobItems, isLoading } = useBookmarksContext();
 
   return (
-    <div className="bookmarks-popover">
+    <div ref={ref} className="bookmarks-popover">
       <JobList jobItems={bookmarkedJobItems} isLoading={isLoading} />
     </div>
   );
-}
+});
+
+export default BookmarksPopover;
